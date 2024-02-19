5dcd00be392ec26dd4e95c7af0cf1b466e07d1f4
Zanko <zanko.mohammadzadeh17@gmail.com>
2024-02-19 11:08:58 +0330

    Add files via upload

---
+++ Task2_SQLcode_03.sql
@@ -0,0 +1,27 @@
+﻿
+-- total days = 419 days 
+select cast(julianday(date(max(createdAt))) -julianday(date(min(createdAt))) + 1 as int) as total_days
+from orders
+
+-- count of holidays = 16 days
+With A as
+(
+select min(date(createdAt)) as min_orderDate, max(date(createdAt)) as max_orderDate
+from orders
+)
+select count(*) as holidays_cnt
+from holidays, A
+where holyDate between min_orderDate and max_orderDate
+
+-- count of non holidays = 403 days
+select count(distinct(date(createdAt))) as CNT_Non_Holi
+from orders
+where date(createdAt) not in 
+(
+select *
+from holidays
+)
+
+
+
+
